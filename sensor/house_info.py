987fe9f980bd8a11532ef4b2e6d06351419a0bcb
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 14:00:34 -0400

    mod 3 done

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -1,3 +1,5 @@
+from datetime import date, datetime
+from time import strftime
 class HouseInfo:
     def __init__(self, data):
         self.data = data
@@ -13,6 +15,14 @@ class HouseInfo:
                 field_data.append(record['area'])
 
         return field_data
+
+    def get_data_by_date(self, field, rec_date = date.today()):
+        field_data = []
+        for record in self.data:
+            if record['date'] == rec_date.strftime("%m/%d/%y"):
+                field_data.append(record[field])
+        return field_data
+
 
     
 
