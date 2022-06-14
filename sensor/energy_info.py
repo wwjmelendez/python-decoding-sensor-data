a342950e67ecaf058af7d8c2f3fbc14de0fb91c0
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 15:55:55 -0400

    .

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -3,7 +3,7 @@ from house_info import HouseInfo
 from datetime import date, datetime
 
 class EnergyData(HouseInfo):
-    ENERGY_PER_BULB = 2.0
+    ENERGY_PER_BULB = 0.2
     ENERGY_BITS = 0x0F0
 
     def _get_energy(self, rec):
