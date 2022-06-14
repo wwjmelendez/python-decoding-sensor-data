1654ade19cae51484745307c697525be796c56c1
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 13:53:39 -0400

    .

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -4,3 +4,16 @@ class HouseInfo:
 
     def get_data_by_area(self, field, rec_area=0):
         field_data = []
+
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record[field])
+
+            elif rec_area == int(record['area']):
+                field_data.append(record['area'])
+
+        return field_data
+
+    
+
+
