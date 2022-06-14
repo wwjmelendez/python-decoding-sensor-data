359597012dfa6b59a18dea1739160aec9e537471
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 15:27:22 -0400

    mod 5 done

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -3,6 +3,7 @@ from load_data import load_sensor_data
 from house_info import HouseInfo
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
+from particle_count_info import ParticleData
 from datetime import date, datetime
 from statistics import mean
 
@@ -58,3 +59,28 @@ print("\tAverrage: {} humidity".format(mean(recs)))
 
 
 # Module 5 code here:
+particle_data = ParticleData(data)
+recs = particle_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
+
+concentrations = particle_data.get_data_concentrations(data=recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+recs = particle_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Particle sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+concentrations = particle_data.get_data_concentrations(data=recs)
+print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
+print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
+print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+
+
+
+
+
+
+
+
+
