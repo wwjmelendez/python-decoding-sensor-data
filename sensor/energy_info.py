8be7a732aa8ca8ed8acd8142057b88db18f731f6
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 15:54:44 -0400

    .

--- sensor/energy_info.py
+++ sensor/energy_info.py
@@ -1,4 +1,6 @@
+from dataclasses import field
 from house_info import HouseInfo
+from datetime import date, datetime
 
 class EnergyData(HouseInfo):
     ENERGY_PER_BULB = 2.0
@@ -9,3 +11,25 @@ class EnergyData(HouseInfo):
         energy = energy & self.ENERGY_BITS 
         energy = energy >> 4
         return energy
+
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+            recs.append(self._get_energy(rec))
+        return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("energy_usage", rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("energy_usage", rec_date)
+        return self._convert_data(recs)
+
+    def calculate_energy_usage(self, data):
+        total_energy = sum(field * self.ENERGY_PER_BULB for field in data)
+        return total_energy
+
+    
+
+
