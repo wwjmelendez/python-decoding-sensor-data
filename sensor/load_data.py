44e1d6657ab255e79839580b859584f36413f62d
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 13:28:25 -0400

    up to 4

---
+++ sensor/load_data.py
@@ -0,0 +1,9 @@
+import os, glob, csv
+
+def load_sensor_data():
+    sensor_date = []
+    sensor_files = glob.glob(os.path.join(os.getcwd(), 'datasets', '*.csv'))
+
+    for sensor_file in sensor_files:
+        with open('sensor_file', 'w') as data_file:
+            data_reader = csv.DictReader(data_file, delimiter=',')
