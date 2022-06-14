475714b76669038e7944ecc6ab00bcd1ed415854
jvrmlndz <melendezdroz@gmail.com>
2022-06-14 13:45:50 -0400

    .

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -2,7 +2,5 @@ class HouseInfo:
     def __init__(self, data):
         self.data = data
 
-    def get_data_by_area(self, field, rec_area):
-        rec_area = 0
+    def get_data_by_area(self, field, rec_area=0):
         field_data = []
-        
